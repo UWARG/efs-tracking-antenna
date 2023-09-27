f75caac6fe8e166c7686fd01b1b9bc585b3483b3
yuchen lin <linyuchen830@gmail.com>
2023-09-26 22:54:27 -0400

    I2C write and read macro

--- Core/Inc/LOS_D_BMX160.hpp
+++ Core/Inc/LOS_D_BMX160.hpp
@@ -2,8 +2,8 @@
 #define INC_BMX160_HPP_
 
 /*----hardware indirect addressing-----*/
-#define MAG_IF_0 0X4C 
-/* 
+#define MAG_IF_0 0X4C
+/*
 MAG_IF_0 {
   <7> mag_manual_enable
   <6> reserved
@@ -34,17 +34,27 @@ typedef struct {
   uint32_t MAG_X;
   uint32_t MAG_Y;
   uint32_t MAG_Z;
-  uint32_t time_stamp;  
+  uint32_t time_stamp;
 } MAG_DATA;
 
 class BMX160{
   public:
-    BMX160();
+    BMX160(I2C_HandleTypeDef *hi2c);
     MAG_DATA Get_MAG_Data();
   private:
     uint32_t _32_BIT_APPEND(uint8_t _31_24, uint8_t _23_16, uint8_t _15_8, uint8_t _7_0) {
       return ((_31_24 << 24) | (_23_16 << 16) | (_15_8 << 8) | (_7_0));
     }
+
+    void I2C_Write(uint16_t addr, uint8_t *p_data) {
+      HAL_I2C_Master_Transmit(HI2C, addr, p_data, 300);
+    }
+
+    void I2C_Read(uint16_t addr, uint8_t *p_data, uint16_t size) {
+      HAL_I2C_Master_Recieve(HI2C, addr, p_data, size, 300);
+    }
+
+    I2C_HandleTypeDef *HI2C;
 }
 
 
